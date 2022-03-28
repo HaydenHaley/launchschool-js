b710afc5ffd4e49798c1fc89c4f27cb2b912f6eb
Hayden Haley <haydenahaley@outlook.com>
2022-03-28 15:54:54 -0400

    Updated calculator

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -1,36 +1,46 @@
 const readline = require('readline-sync');
 
-console.log("Welcome to Calculator!");
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
 
-console.log("What's the first number?");
+function isValidNumber(number){
+  return !(number.trimStart() === '' || Number.isNaN(Number(number)));
+}
+
+prompt("Welcome to Calculator!");
+
+prompt("What's the first number?");
 let firstNumber = readline.questionFloat();
-console.log(`Great! You entered ${firstNumber}.`);
+prompt(`Great! You entered ${firstNumber}.`);
 
-console.log("What's the second number?");
+prompt("What's the second number?");
 let secondNumber = readline.questionFloat();
-console.log(`Great! You entered ${secondNumber}.`);
+prompt(`Great! You entered ${secondNumber}.`);
 
-console.log('What operation would you like to perform?\n1) Add \n2) Subtract \n3) Multiply \n4) Divide')
-let operation = readline.questionInt();
-
+let operation;
 let output;
 
-switch (operation){
-  case 1:
-    output = firstNumber + secondNumber;
-    break;
-  case 2:
-    output = firstNumber - secondNumber;
-    break;
-  case 3:
-    output = firstNumber * secondNumber;
-    break;
-  case 4:
-    output = firstNumber / secondNumber;
-    break;
-  default: console.log('Sorry, that\'s not a valid operation type.');
-}
-if (typeof output === 'undefined')
-  console.log('The program could not complete with the given input. Please try again.');
-else
-  console.log(`Result: ${output}.`);
+while (output === undefined) {
+  prompt('What operation would you like to perform?\n1) Add \n2) Subtract \n3) Multiply \n4) Divide');
+  operation = readline.questionInt();
+  switch (operation) {
+    case 1:
+      output = firstNumber + secondNumber;
+      break;
+    case 2:
+      output = firstNumber - secondNumber;
+      break;
+    case 3:
+      output = firstNumber * secondNumber;
+      break;
+    case 4:
+      output = firstNumber / secondNumber;
+      break;
+    default: 
+      prompt('Sorry, that\'s not a valid operation type.');
+      break;
+  }
+} 
+
+prompt(`Result: ${output}.`);
